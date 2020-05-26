cfd0802907d2cec17411b26d542563472261b017
Justin Meyer <meyerj.justin@yahoo.com>
2020-05-25 22:14:48 -0400

    W2D2 pre lecture

---
+++ W2D2/objectLecture.js
@@ -0,0 +1,62 @@
+// The Object Type
+// POJO- Plain Old JavaScript Object
+  // An unordered collection of key-value pairs
+
+// Keys are strings and must be unique
+// Keys point to values.
+// Values can be anything!
+// Objects are unordered, order is not guaranteed!
+
+
+
+// -------------------------------------------------------
+
+// Initializing an empty object
+// Initializing an object with multiple key-value pairs.
+
+let emptyObject = {};
+
+let instructorObject = {
+  g: 'Gordon',
+  s: 'Soon-Mi',
+  a: 'Angela'
+};
+
+// console.log(emptyObject);
+// console.log(instructorObject);
+
+
+
+
+
+
+
+// ----------------------------------------------
+
+// Adding key-value pairs to an object.
+
+
+let key = 'j';
+instructorObject[key] = 'Justin';
+
+instructorObject.r = 'Rose';
+
+
+
+
+
+
+
+// console.log('\ninstructorObject: \n');
+// console.log(instructorObject);
+// console.log('\n--------- \n \nValue under "g" key: \n');
+// console.log(instructorObject['g']);
+// console.log('\n--------- \n \nValue under "s" key: \n');
+// console.log(instructorObject.s);
+// console.log('\n--------- \n \nValue under "a" key: \n');
+// console.log(instructorObject.a);
+// console.log('\n--------- \n \nValue under "j" key: \n');
+// console.log(instructorObject['j']);
+// console.log('\n--------- \n \nValue under "r" key: \n');
+// console.log(instructorObject['r']);
+// console.log('\n---------');
