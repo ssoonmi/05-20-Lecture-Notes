2eab87fda7d975b6e45eed178e3538382278316f
Soon-Mi Sugihara <ssoonmi@gmail.com>
2020-07-28 11:17:38 -0400

    Add first W11D1 lecture notes

---
+++ W11D2/README.md
@@ -0,0 +1,7 @@
+# Intro to Express and Pug
+
+## Intro to Express
+
+[Intro to Express Notes]
+
+[Intro to Express Notes]: ./intro-to-express.md
---
+++ W11D2/intro-to-express.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+
+app.get(`/hello`, (req, res) => {
+  res.send('<h1>GET /hello</h1>');
+});
+
+app.get(`/`, (req, res) => {
+  res.send('<h1>GET /</h1>');
+});
+
+app.get(`/blogs/:id`, (req, res) => {
+  res.send(`
+    <h1>GET /blogs/:id</h1>
+    <h2>:id is ${req.params.id}</h2>
+  `);
+});
+
+app.get(`/blogs/hello`, (req, res) => {
+  res.send('<h1>GET /blogs/hello</h1>');
+});
+
+app.get(`/blogs/:id/comments`, (req, res) => {
+  res.send(`
+    <h1>GET /blogs/:id/comments</h1>
+    <h2>:id is ${req.params.id}</h2>
+  `);
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log('Server is listening on port', port));
