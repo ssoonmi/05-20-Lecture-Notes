342478a5a02f89b64cf20faa762ba3dbbea2a837
Soon-Mi Sugihara <ssoonmi@gmail.com>
2020-07-28 12:50:59 -0700

    Add rest of W11D2 lecture notes

--- W11D2/intro-to-express.js
+++ W11D2/intro-to-express.js
@@ -7,18 +7,20 @@ app.get(`/hello`, (req, res) => {
 });
 
 app.get(`/`, (req, res) => {
+  debugger
   res.send('<h1>GET /</h1>');
 });
 
+app.get(`/blogs/hello`, (req, res) => {
+  res.send("<h1>GET /blogs/hello</h1>");
+});
+
 app.get(`/blogs/:id`, (req, res) => {
+  console.log('params', req.params);
   res.send(`
     <h1>GET /blogs/:id</h1>
     <h2>:id is ${req.params.id}</h2>
   `);
-});
-
-app.get(`/blogs/hello`, (req, res) => {
-  res.send('<h1>GET /blogs/hello</h1>');
 });
 
 app.get(`/blogs/:id/comments`, (req, res) => {
--- W11D2/render-html-templates/app.js
+++ W11D2/render-html-templates/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const port = 8081;
+
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  res.render('layout', { title: 'Welcome', heading: 'Home' });
-});
+// routes
 
-const port = 8081;
+app.get('*', (req, res) => {
+  res.render('error', { title: 'Welcome', heading: 'Home' });
+});
 
 app.listen(port, () => console.log('Server is listening on port', port));
