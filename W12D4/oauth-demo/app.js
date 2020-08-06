8a00c55bec1174b0ca98c3520ef0e21823df33d8
Soon-Mi Sugihara <ssoonmi@gmail.com>
2020-08-06 12:09:51 -0700

    Add to W12D4 lecture notes

--- W12D4/oauth-demo/app.js
+++ W12D4/oauth-demo/app.js
@@ -33,22 +33,39 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  console.log(req.session);
-  if (req.cookies.token) {
-    const { email, id } = jwt.verify(req.cookies.token, jwtConfig.secret);
-    const href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleAuth.clientId}&redirect_uri=http://localhost:3000/oauth/success&response_type=code&scope=email`;
-    res.render('index', { href, email });
-  } else {
-    const href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleAuth.clientId}&redirect_uri=http://localhost:3000/oauth/success&response_type=code&scope=email`;
-    res.render("index", { href });
+app.get('/', async (req, res) => {
+  // If there is a JWT auth token in the cookie, find a user by the id in the
+    // payload of that JWT. If JWT auth token cannot be verified or no user is
+    // found, clear the cookie to log out the user from the session
+  try {
+    if (req.cookies.token) {
+      const { id } = jwt.verify(req.cookies.token, jwtConfig.secret);
+
+      const user = await User.findByPk(id);
+      if (!user) throw new Error('No user found with that id');
+
+      return res.render('index', { 
+        email: user.email, 
+        id: user.id, 
+        loggedIn: true
+      });
+    } 
+  } catch(err) {
+    res.clearCookie('token');
   }
+
+  // If there is no user found in the session, then prompt the user to sign in
+  const href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleAuth.clientId}&redirect_uri=http://localhost:3000/oauth/success&response_type=code&scope=email`;
+  res.render("index", { href, loggedIn: false });
 });
 
 app.get('/oauth/success', async (req, res) => {
-  console.log(req.headers);
-  console.log(req.query);
   const { code } = req.query;
+  // Google OAuth gives the code to you in the query
+  // Make a POST request to Google OAuth with that code and your Google Client
+    // Id and your Google Client Secret to get information about the signed in
+    // Google user and a token to make more requests on behalf of that user to
+    // Google
   const data = await fetch("https://oauth2.googleapis.com/token", {
     method: "POST",
     body: JSON.stringify({
@@ -59,6 +76,9 @@ app.get('/oauth/success', async (req, res) => {
       redirect_uri: 'http://localhost:3000/oauth/success'
     })
   });
+
+  // From the response, you can extract the id_token which is a JWT containing
+    // information about the user like their email
   const { 
     access_token, 
     expires_in, 
@@ -66,6 +86,8 @@ app.get('/oauth/success', async (req, res) => {
     token_type, 
     id_token 
   } = await data.json();
+
+  // Extract the email from the JWT payload
   const {
     iss,
     azp,
@@ -78,22 +100,30 @@ app.get('/oauth/success', async (req, res) => {
     exp
   } = jwt.decode(id_token);
 
+  // Find a user by the email provided by Google
   let user = await User.findOne({
     where: { email }
   });
+
+  // If no user is found by that email, create the user with that email
   if (!user) {
     user = await User.create({
       email
     });
   }
 
+  // Create an JWT authentication token for your app to use to authorize and
+    // identify this user
   const token = jwt.sign(
     { email: user.email, id: user.id },
     jwtConfig.secret,
     { expiresIn: jwtConfig.expiresIn } // 604,800 seconds = 1 week
   );
 
+  // Set the JWT auth token to the cookie
   res.cookie("token", token, { httpOnly: true });
+
+  // Redirect the user to the home page
   res.redirect('/');
 });
 
