26b8df59c8099b6807bb723df12b2f0dea4e41e9
Justin Meyer <meyerj.justin@yahoo.com>
2020-08-26 21:03:05 -0400

    debugging add tweet pt 3

--- W15D3/twitter-backend/routes/utils/auth.js
+++ W15D3/twitter-backend/routes/utils/auth.js
@@ -26,6 +26,7 @@ exports.getUserFromToken = async (token) => {
 }
 
 exports.requireUser = (req, res, next) => {
+  console.log('Current User', req.user);
   if (req.user) return next();
   const err = Error("Unauthorized");
   err.status = 401;
